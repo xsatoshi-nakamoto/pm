99785953054cf6ca9f9db4a8e28dd45d55625daa
xsatoshi-nakamoto <100011005+xsatoshi-nakamoto@users.noreply.github.com>
2022-02-19 11:43:21 +0700

    Add files via upload

---
+++ jiramodels/project.go
@@ -0,0 +1,12 @@
+package jiramodels
+
+type Project struct {
+	ID           int64
+	PNAME        string
+	LEAD         string
+	description  string
+	pkey         string
+	PCOUNTER     int64
+	ASSIGNEETYPE int64
+	AVATAR       int64
+}
